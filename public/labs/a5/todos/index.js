f60f8a01d6fd16edfb25c72b791abb79e3e79d21
tyrannorrec <92607420+tyrannorrec@users.noreply.github.com>
2023-02-16 12:35:20 -0500

    Assignment 5 -- initial upload

--- public/labs/a5/todos/index.js
+++ public/labs/a5/todos/index.js
@@ -3,7 +3,7 @@ import TodoList from "./TodoList.js"
 // alert('Hello World!');
 
 $('#wd-todo').append(`
-   <div class="container">
+   <div class="container ms-0">
        <h1>Todo example</h1>
        ${TodoList()}
    </div>
