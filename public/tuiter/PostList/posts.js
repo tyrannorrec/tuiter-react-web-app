8dde04616342873662ff4ceb20e50dfb9fa6cf8f
tyrannorrec <92607420+tyrannorrec@users.noreply.github.com>
2023-02-16 12:24:36 -0500

    Assignment 5 -- initial upload

---
+++ public/tuiter/PostList/posts.js
@@ -0,0 +1,36 @@
+export default [
+    {
+        userName: 'New Discourses',
+        handle: 'NewDiscourses',
+        time: 'Sep 14',
+        profilePicture: '../images/profile-picture-new-discourses.png',
+        tuit: 'Nam nec sem sit amet velit posuere ullamcorper et at eros.',
+        image: '../images/image-3.jpeg',
+        title: ' Lorem ipsum dolor sit amet | Consectetur Adipiscing Elit',
+        textSummary: 'Curabitur quis eros suscipit lectus gravida finibus in sed magna. Nullam maximus ex non ' +
+            'ultrices tincidunt ...',
+        link: 'https://www.lipsum.com/',
+        linkShort: 'lipsum.com',
+        comments: '4.2K',
+        retuits: '3.5K',
+        likes: '37.5K',
+    },
+    {
+        userName: 'Brandon',
+        handle: 'brandon',
+        time: 'Aug 22',
+        profilePicture: '../images/profile-picture-brandon.jpeg',
+        tuit: 'Ut mi tortor, tincidunt venenatis leo nec, vulputate mattis erat. Nulla consectetur ut risus ' +
+            'ac scelerisque. ',
+        image: '../images/image-5.jpeg',
+        title: 'Pellentesque vitae eros ligula. Maecenas fringilla... ',
+        textSummary: 'Quisque quis est vulputate, malesuada velit euismod, eleifend arcu. Ut auctor vitae ' +
+            'lorem eget hendrerit ...',
+        link: 'https://www.tumblr.com/',
+        linkShort: 'tumblr.com',
+        comments: '965',
+        retuits: '2.4K',
+        likes: '4K',
+    },
+
+];
